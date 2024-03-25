bd594a6b25d8850afb776fc1116c109918893c22
oceaniswater <markflyingbear@gmail.com>
2024-03-25 18:06:35 +0000

    create app coordinator

--- MoneyBox/App/AppCoordinator/AppCoordinator.swift
+++ MoneyBox/App/AppCoordinator/AppCoordinator.swift
@@ -5,4 +5,24 @@
 //  Created by Mark Golubev on 25/03/2024.
 //
 
-import Foundation
+import UIKit
+
+protocol Coordinator {
+    var parentCoordinator: Coordinator? { get set }
+    var children: [Coordinator] { get set }
+    var navigationController : UINavigationController { get set }
+    
+    func start()
+}
+
+class AppCoordinator: Coordinator {
+    var parentCoordinator: Coordinator?
+    var children: [Coordinator] = []
+    var navigationController: UINavigationController
+    init(navCon : UINavigationController) {
+        self.navigationController = navCon
+    }
+    func start() {
+        print("App Coordinator Start")
+    }
+}
