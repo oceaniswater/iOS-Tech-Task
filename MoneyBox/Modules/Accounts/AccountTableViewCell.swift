47730a91fdfffe91bebda48f61ee0bc6df3f4dbc
oceaniswater <markflyingbear@gmail.com>
2024-03-26 21:51:30 +0000

    add accounts table view + cell

--- MoneyBox/Modules/Accounts/AccountTableViewCell.swift
+++ MoneyBox/Modules/Accounts/AccountTableViewCell.swift
@@ -6,18 +6,145 @@
 //
 
 import UIKit
+import Networking
 
 class AccountTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
+    public static var identifier: String {
+        get {
+            return "AccountTableViewCell"
+        }
+    }
+    
+    // MARK: - Init
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCell()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private properties
+    private let view: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 5
+//        view.layer.shadowColor = K.Design.separatorLineColor?.cgColor
+        view.layer.shadowOpacity = 1
+        view.layer.shadowOffset = CGSize(width: 0, height: 2)
+        view.layer.shadowRadius = 1
+        return view
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+//        label.textColor = K.Design.primaryTextColor
+        return label
+    }()
+    
+    private let planLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+//        label.textColor = K.Design.secondTextColor
+        return label
+    }()
+    
+    private let moneyboxLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+//        label.textColor = K.Design.secondTextColor
+        return label
+    }()
+    
+    private var vStack: UIStackView!
+    private var hStack: UIStackView!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        nameLabel.text      = nil
+        planLabel.text      = nil
+        moneyboxLabel.text  = nil
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        backgroundColor = .clear
+    }
+    
+    // MARK: - Public methods
+    func configure(with item: ProductResponse) {
+        nameLabel.text          = item.product?.name
+        planLabel.text          = "Plan Value: \(item.planValue ?? 0.00)"
+        moneyboxLabel.text      = "Moneybox: \(item.moneybox ?? 0.00)"
+    }
 }
+
+// MARK: - Setup Cell
+private extension AccountTableViewCell {
+    func setupCell() {
+        backgroundColor = .clear
+        
+        addSubview()
+        setupLayout()
+    }
+}
+
+// MARK: - Setting
+private extension AccountTableViewCell {
+    func addSubview() {
+        addSubview(view)
+        
+        vStack = UIStackView(arrangedSubviews: [nameLabel, planLabel, moneyboxLabel])
+        vStack.axis = .vertical
+        vStack.spacing = 5
+        vStack.alignment = .leading
+        vStack.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(vStack)
+    }
+}
+
+// MARK: - Setup Layout
+private extension AccountTableViewCell {
+    func setupLayout() {
+        NSLayoutConstraint.activate([
+            view.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            view.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            view.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
+            view.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
+            view.heightAnchor.constraint(equalToConstant: 100),
+            
+            nameLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            nameLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            planLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 4),
+            planLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            planLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            planLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            moneyboxLabel.topAnchor.constraint(equalTo: planLabel.bottomAnchor, constant: 4),
+            moneyboxLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            moneyboxLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            moneyboxLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            vStack.topAnchor.constraint(equalTo: view.topAnchor, constant: 8),
+            vStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            vStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            vStack.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -8)
+        ])
+    }
+}
+
--- MoneyBox/Modules/Accounts/AccountViewController+TableView.swift
+++ MoneyBox/Modules/Accounts/AccountViewController+TableView.swift
@@ -1,8 +1,49 @@
 //
-//  AccountViewController+TableView.swift
+//  AccountsViewController+TableView.swift
 //  MoneyBox
 //
 //  Created by Mark Golubev on 26/03/2024.
 //
 
 import Foundation
+import UIKit
+
+// MARK: - UITableViewDelegate, UITableViewDataSource
+extension AccountsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func setupTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.registerTableCell()
+    }
+    
+    func registerTableCell() {
+        tableView.register(AccountTableViewCell.self, forCellReuseIdentifier: AccountTableViewCell.identifier)
+    }
+    
+    func reloadTableView() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        viewModel?.numberOfSections() ?? 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel?.numberOfRows(in: section) ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: AccountTableViewCell.identifier, for: indexPath) as? AccountTableViewCell else { return UITableViewCell() }
+        
+        guard let item = self.viewModel?.products[indexPath.row] else { return UITableViewCell() }
+        cell.configure(with: item)
+        cell.selectionStyle = .none
+        
+        return cell
+        
+    }
+}
